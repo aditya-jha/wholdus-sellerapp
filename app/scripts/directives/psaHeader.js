115e7f863891b60da7b62c5d2953eb23c656ab5a
Aditya Jha <batu.aditya007@gmail.com>
2016-04-25 08:08:49 +0530

    added  directive to self exectuing anonymous function

--- app/scripts/directives/psaHeader.js
+++ app/scripts/directives/psaHeader.js
@@ -1,15 +1,18 @@
-sellerapp.directive('psaHeader', function() {
-    return {
-        restrict: 'AE',
-        templateUrl: 'views/directives/psaHeader.html',
-        link: function(scope, element, attributes) {
-        },
-        controller: [
-            '$scope',
-            '$log',
-            function($scope, $log) {
-                $log.log("loaded");
-            }
-        ]
-    };
-});
+(function() {
+    "use strict";
+    sellerapp.directive('psaHeader', function() {
+        return {
+            restrict: 'AE',
+            templateUrl: 'views/directives/psaHeader.html',
+            link: function(scope, element, attributes) {
+            },
+            controller: [
+                '$scope',
+                '$log',
+                function($scope, $log) {
+                    $log.log("loaded");
+                }
+            ]
+        };
+    });
+})();
