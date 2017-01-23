6ccd1dfaeeeb29f25848903a9b5bdadce9a9baa5
Aditya Jha <adityaiitr@hotmail.com>
2017-01-23 09:21:28 +0530

    changes

--- app/scripts/services/constantKeyValueService.js
+++ app/scripts/services/constantKeyValueService.js
@@ -5,8 +5,8 @@
             var formValidation=FormValidationService;
             var factory = {
                 token: '',
-                //apiBaseUrl: 'http://api.wholdus.com/',
-                apiBaseUrl: 'http://13.76.211.119:8000/',
+                apiBaseUrl: 'http://api.wholdus.com/',
+                //apiBaseUrl: 'http://13.76.211.119:8000/',
                 apiUrl: {
                     login: 'admin/login',
                     internalUsers: 'users/internal-users',
